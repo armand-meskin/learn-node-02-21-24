0710bf7d84f85e5447dc7aff8ff6dca5e3c164c4
armand-meskin <armandm101@gmail.com>
2024-02-28 11:49:43 -0500

    Completed

--- eventloop/quiz/immediate.js
+++ eventloop/quiz/immediate.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
+setImmediate(() => {
+  console.log('I was scheduled to run immediately');
+});
+
 fs.readFile('./data/test.txt', function(err, data) {
   const startCallback = Date.now();
   // do something that will take 10ms...
   while (Date.now() - startCallback < 10) {
     // do nothing
   }
+
   if (err) console.log('Error');
   else console.log(data);
 
@@ -15,9 +20,5 @@ fs.readFile('./data/test.txt', function(err, data) {
 
     console.log(`${delay}ms have passed since I was scheduled`);
   }, 5);
-
-  setImmediate(() => {
-    console.log('I was scheduled to run immediately');
-  });
 });
 
